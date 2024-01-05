9a2513526c17bd2af99a5ada653c0c1b92f832c6
Pranavlovescode <pranavtitambe04@gmail.com>
2024-01-05 22:04:00 +0530

    Updated coins details with realtime API

--- src/components/navbar/Home.js
+++ src/components/navbar/Home.js
@@ -1,26 +1,24 @@
-import React from "react";
-import coinDetail from "./coinDetail";
+import React, { useEffect, useState } from "react";
+// import coinDetail from "./coinDetail";
 function Home() {
-  
-    const data=coinDetail.map((index) => (
-      <ul key={index.id} className="list-group">
-        <li className={`list-group-item d-flex justify-content-between align-items-start text-white ${index.mc>=0?'bg-success':'bg-danger'}`}>
-          <div className="ms-2 me-auto">
-            <div className="fw-bold">
-              {index.name} ({index.s_name})
-            </div>
-            {index.mc} %
-          </div>
-          <span className="mt-2">$ {index.price}</span>
-        </li>
-      </ul>
-    ));
+  const [crypto, setCrypto] = useState([]);
+  useEffect(() => {
+    async function fetchData() {
+      const response = await fetch(
+        "https://api.coingecko.com/api/v3/coins/markets?vs_currency=inr&order=market_cap_desc&per_page=10&page=1&sparkline=false&locale=en&precision=2"
+      );
+      const data = await response.json();
+      setCrypto(data)
+      console.log(data);      
+    }
+    fetchData()
+  }, []);
 
   return (
     <>
-      <div className="row p-2">
-        <div className="col-12 col-lg-5">
-          <div className="cointainer">
+      <div className="row cointainer-fluid">
+        <div className="col-12 col-lg-5 ">
+          <div className="cointainer-fluid">
             <h2>Bitcoin</h2>
             <p>
               Bitcoin is the first decentralized cryptocurrency. Nodes in the
@@ -30,7 +28,23 @@ function Home() {
             </p>
           </div>
         </div>
-        <div className="col-12 col-lg-7">{data}</div>
+        <div className="col-12 col-lg-7">{crypto.map((index) => (
+        <ul key={index.id} className="list-group">
+          <li
+            className={`list-group-item d-flex justify-content-between align-items-start text-white ${
+              index.market_cap_change_percentage_24h >= 0 ? "bg-success" : "bg-danger"
+            }`}
+          >
+            <div className="ms-2 me-auto">
+              <div className="fw-bold">
+                {index.name} ({index.symbol})
+              </div>
+              {index.market_cap_change_percentage_24h} %
+            </div>
+            <span className="mt-2">&#x20b9; {index.current_price}</span>
+          </li>
+        </ul>
+      ))}</div>
       </div>
     </>
   );
